e7dd2b4c5fcd0db5d496cc8d0a792f920da07fd4
alpesh_pandya <alpesh_pandya@apple.com>
2018-03-05 22:19:27 -0600

    Changes in readme and adding arguments to program

--- kafka-python-producer/README.md
+++ kafka-python-producer/README.md
@@ -15,11 +15,16 @@ This producer includes sample data from citybikes. It will produce one event eve
 * `brew install kafka`
 * `brew services start zookeeper`
 * `brew services start kafka`
-* 'kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic citybikes'
-* Optional (if you want to see action): 'kafka-console-consumer --bootstrap-server localhost:9092 --topic citybikes --from-beginning'
+* `kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic citybikes`
+* Optional (if you want to see action): `kafka-console-consumer --bootstrap-server localhost:9092 --topic citybikes --from-beginning`
 
 ## Instructions
 * Clone the repo
 * Open concole in kafka-python-producer directory
 * Run the command: `python kafka_producer.py`
 * Go to console where consumer is running to see the magic
+
+## optional arguments
+* -f , --file   Sample data file path
+* -k , --kafkaServers Comma separated list of Kafka hosts
+* -t , --topic Name of topic
--- kafka-python-producer/kafka_producer.py
+++ kafka-python-producer/kafka_producer.py
@@ -3,13 +3,21 @@ import json
 import csv
 import time
 import os
+import argparse
+
 if __name__ == '__main__':
-    currentPath = os.path.dirname(os.path.realpath(__file__))
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    reader = csv.DictReader(open(currentPath+'/sample_citybikes.csv', 'r'))
+    currentPath = os.path.dirname(os.path.realpath(__file__))+'/sample_citybikes.csv'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='Sample data file path',default=currentPath)
+    parser.add_argument('-k', '--kafkaServers', help='Comma separated list of Kafka hosts',default='localhost:9092')
+    parser.add_argument('-t', '--topic', help='Name of topic',default='citybikes')
+    args = parser.parse_args()
+
+    producer = KafkaProducer(bootstrap_servers=args.kafkaServers)
+    reader = csv.DictReader(open(args.file, 'r'))
     messageCount=0
     for line in reader:
-        producer.send('citybikes', json.dumps(line).encode('utf-8'))
+        producer.send(args.topic, json.dumps(line).encode('utf-8'))
         messageCount=messageCount+1
         print ("Sent: "+str(messageCount))
         time.sleep(1)
