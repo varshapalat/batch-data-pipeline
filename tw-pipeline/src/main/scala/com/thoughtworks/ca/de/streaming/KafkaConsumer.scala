ad7b2ac7694d8f7125a6ba5ec13ca4e2cb253208
alpesh_pandya <alpesh_pandya@apple.com>
2018-03-12 15:23:07 -0400

    Read from the stream

--- tw-pipeline/src/main/scala/com/thoughtworks/ca/de/streaming/KafkaConsumer.scala
+++ tw-pipeline/src/main/scala/com/thoughtworks/ca/de/streaming/KafkaConsumer.scala
@@ -2,7 +2,13 @@ package com.thoughtworks.ca.de.streaming
 
 import com.typesafe.config.ConfigFactory
 import org.apache.log4j.LogManager
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DateType, TimestampType}
+import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
 object KafkaConsumer {
   def main(args: Array[String]): Unit = {
@@ -11,6 +17,7 @@ object KafkaConsumer {
     val spark = SparkSession.builder
       .appName("StructuredNetworkWordCount")
       .getOrCreate()
+    import spark.implicits._
 
     /*
     Writer code here to:
@@ -21,6 +28,51 @@ object KafkaConsumer {
     5. Count number of rides per minute window
     6. Write to file system/hdfs in parquet format partitioned by minute window
      */
+    val schema = DataTypes.createStructType(
+      Array[StructField](
+        DataTypes
+          .createStructField("tripduration", DataTypes.StringType, false),
+        DataTypes.createStructField("starttime", DataTypes.StringType, false),
+        DataTypes.createStructField("stoptime", DataTypes.StringType, false),
+        DataTypes
+          .createStructField("start station id", DataTypes.StringType, false),
+        DataTypes
+          .createStructField("start station name", DataTypes.StringType, false),
+        DataTypes.createStructField("start station latitude",
+          DataTypes.StringType,
+          false),
+        DataTypes
+          .createStructField("end station id", DataTypes.StringType, false),
+        DataTypes
+          .createStructField("end station name", DataTypes.StringType, false),
+        DataTypes.createStructField("end station latitude",
+          DataTypes.StringType,
+          false),
+        DataTypes.createStructField("end station longitude",
+          DataTypes.StringType,
+          false),
+        DataTypes.createStructField("bikeid", DataTypes.StringType, false),
+        DataTypes.createStructField("usertype", DataTypes.StringType, false),
+        DataTypes.createStructField("birth year", DataTypes.StringType, false),
+        DataTypes.createStructField("gender", DataTypes.StringType, false)
+      ))
 
+    val bikesDF = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers",
+        conf.getString("streaming.kafka.hosts"))
+      .option("subscribe", conf.getString("streaming.kafka.topic"))
+      .option("startingOffsets", "earliest")
+      .load()
+      .selectExpr("CAST(value AS STRING) as message")
+      .select(functions.from_json(functions.col("message"), schema).as("json"))
+      .select("json.*")
+      .withColumn("starttime", ($"starttime".cast(TimestampType)))
+      .withColumn("stoptime", ($"stoptime".cast(TimestampType)))
+      .withColumn("birth year", ($"birth year".cast(DateType)))
+
+    bikesDF.printSchema()
+
+    
   }
 }
