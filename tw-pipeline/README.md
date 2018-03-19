3cc29a9fb53d35bd94ce31da18cf292dd1c798a8
alpesh_pandya <alpesh_pandya@apple.com>
2018-03-19 17:07:40 -0500

    Adding word count app and spark 2 cheat sheet

--- tw-pipeline/README.md
+++ tw-pipeline/README.md
@@ -28,3 +28,13 @@
 * Ingest data to datalake: `spark-submit --jars config-1.3.2.jar --class com.thoughtworks.ca.de.batch.ingest.DailyDriver --master local target/scala-2.11/tw-pipeline_2.11-0.1.0-SNAPSHOT.jar`
 * Transform data : `spark-submit --jars config-1.3.2.jar --class com.thoughtworks.ca.de.batch.transform.DailyDriver --master local target/scala-2.11/tw-pipeline_2.11-0.1.0-SNAPSHOT.jar`
 * Sample App: `spark-submit --jars config-1.3.2.jar --class com.thoughtworks.ca.de.batch.app.UberRidesByHumidityRange --master local twde-capabilities/tw-pipeline/target/scala-2.11/tw-pipeline_2.11-0.1.0-SNAPSHOT.jar`
+
+## Instructions to install airflow (local)
+* Install Python 2.7 as airflow is on compatible with Python 3 yet: `pip install python@2`
+* Install virtual environment: `pip install virtualenv`
+* Create vm for airflow: `virtualenv airflow_vm`
+* `cd airflow_vm/`
+* `source bin/activate`
+* `pip install airflow`
+* `airflow initdb`
+* `airflow webserver -p 9095`
---
+++ tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/app/WordCount.scala
@@ -0,0 +1,43 @@
+package com.thoughtworks.ca.de.batch.app
+
+import com.thoughtworks.ca.de.common.utils.DateUtils
+import org.apache.spark.sql.SparkSession
+import com.typesafe.config.ConfigFactory
+import org.apache.log4j.{Level, LogManager}
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = ConfigFactory.load
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.INFO)
+    val spark =
+      SparkSession.builder.appName("Spark 2 Word Count").getOrCreate()
+    log.info("Application Initialized: " + spark.sparkContext.appName)
+
+    //Parse argument/s
+    var processingDate = DateUtils.date2TWFormat()
+    if (!args.isEmpty) {
+      processingDate = DateUtils.parseISO2TWFormat(args(0))
+    }
+
+    val outputPath = conf.getString("apps.WordCount.output").format(processingDate)
+
+    import spark.implicits._
+
+    log.info("Reading data: " + conf.getString("apps.WordCount.input"))
+    log.info("Writing data: " + outputPath)
+
+    spark.read
+      .text(conf.getString("apps.WordCount.input")) // Read file
+      .as[String]                                   // As a data set
+      .flatMap(text => text.split("\\s+"))          // Split words using regex (distributed map)
+      .groupByKey(_.toLowerCase())                  // Reduce action
+      .count()                                      // Aggregate
+      .withColumnRenamed("count(1)","count")        // Cannot save data with column name count(1)
+      .write                                        // Write to file
+      .csv(outputPath)
+
+    log.info("Application Done: " + spark.sparkContext.appName)
+    spark.stop()
+
+  }
+}
