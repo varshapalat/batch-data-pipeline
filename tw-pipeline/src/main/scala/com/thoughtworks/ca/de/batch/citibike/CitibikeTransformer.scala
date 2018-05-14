13294edfdf28d91c16ecc86ca2d22623cd185205
Alex Jablonski <ajablonski@thoughtworks.com>
2018-05-14 17:21:36 -0500

    [Alex] - Add UDF implementation of distance column

--- tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/citibike/CitibikeTransformer.scala
+++ tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/citibike/CitibikeTransformer.scala
@@ -2,6 +2,7 @@ package com.thoughtworks.ca.de.batch.citibike
 
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions._
 
 object CitibikeTransformer {
@@ -27,19 +28,42 @@ object CitibikeTransformer {
     spark.stop()
   }
 
-  def run(sparkSession: SparkSession, ingestPath: String, outputPath: String): Unit = {
-    sparkSession
-      .read
+  private final val MetersPerFoot = 0.3048
+  private final val FeetPerMile = 5280
+
+  final val EarthRadiusInM: Double = 6371e3
+  final val MetersPerMile: Double = MetersPerFoot * FeetPerMile
+
+  def distanceFromLatitudeLongitudeUdf: UserDefinedFunction =
+    udf((lat1: Double, lon1: Double, lat2: Double, lon2: Double) => {
+      val Δlatitude = (lat2 - lat1).toRadians
+      val Δlongitude = (lon2 - lon1).toRadians
+
+      val a = Math.pow(Math.sin(Δlatitude / 2), 2) +
+        Math.cos(lat1.toRadians) * Math.cos(lat2.toRadians) *
+          Math.pow(Math.sin(Δlongitude / 2), 2)
+
+      val radianDistance = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+      val distanceInMeters = EarthRadiusInM * radianDistance
+      val distanceInMiles = distanceInMeters / MetersPerMile
+
+      distanceInMiles
+    })
+
+  def run(sparkSession: SparkSession,
+          ingestPath: String,
+          outputPath: String): Unit = {
+    sparkSession.read
       .parquet(ingestPath)
-      .withColumn("delta_lat", radians(col("end_station_latitude")) - radians(col("start_station_latitude")))
-      .withColumn("delta_long", radians(col("end_station_longitude")) - radians(col("end_station_longitude")))
-      .withColumn("arch", sin(col("delta_lat") / 2) * sin(col("delta_lat") / 2) +
-        cos(radians(col("start_station_latitude"))) * cos(radians(col("end_station_latitude"))) *
-          sin(col("delta_long") / 2) * sin(col("delta_long") / 2))
-      .withColumn("curve", atan2(sqrt(col("arch")), sqrt(-col("arch") + 1)) * 2)
-      .withColumn("distance_miles", col("curve") * 6371000 / 1609.34)
-      .withColumn("distance", round(col("distance_miles"), 2))
-      .drop("delta_lat", "delta_long", "arch", "curve", "distance_miles")
+      .withColumn(
+        "distance",
+        round(distanceFromLatitudeLongitudeUdf(col("start_station_latitude"),
+          col("start_station_longitude"),
+          col("end_station_latitude"),
+          col("end_station_longitude")),
+          scale = 2)
+      )
       .write
       .parquet(outputPath)
   }
--- tw-pipeline/src/test/scala/com/thoughtworks/ca/de/batch/citibike/CitibikeTransformerTest.scala
+++ tw-pipeline/src/test/scala/com/thoughtworks/ca/de/batch/citibike/CitibikeTransformerTest.scala
@@ -73,9 +73,9 @@ class CitibikeTransformerTest extends DefaultFeatureSpecWithSpark {
         .parquet(transformedDir.toUri.toString)
 
       val expectedData = Array(
-        Row(328, "2017-07-01 00:00:08", "2017-07-01 00:05:37", 3242, "Schermerhorn St & Court St", 40.69102925677968, -73.99183362722397, 3397, "Court St & Nelson St", 40.6763947, -73.99869893, 27937, "Subscriber", 1984, 2, 1.01),
-        Row(1496, "2017-07-01 00:00:18", "2017-07-01 00:25:15", 3233, "E 48 St & 5 Ave", 40.75724567911726, -73.97805914282799, 546, "E 30 St & Park Ave S", 40.74444921, -73.98303529, 15933, "Customer", 1971, 1, 0.88),
-        Row(1067, "2017-07-01 00:16:31", "2017-07-01 00:34:19", 448, "W 37 St & 10 Ave", 40.75660359, -73.9979009, 487, "E 20 St & FDR Drive", 40.73314259, -73.97573881, 27084, "Subscriber", 1990, 2.0, 1.62)
+        Row(328, "2017-07-01 00:00:08", "2017-07-01 00:05:37", 3242, "Schermerhorn St & Court St", 40.69102925677968, -73.99183362722397, 3397, "Court St & Nelson St", 40.6763947, -73.99869893, 27937, "Subscriber", 1984, 2, 1.07),
+        Row(1496, "2017-07-01 00:00:18", "2017-07-01 00:25:15", 3233, "E 48 St & 5 Ave", 40.75724567911726, -73.97805914282799, 546, "E 30 St & Park Ave S", 40.74444921, -73.98303529, 15933, "Customer", 1971, 1, 0.92),
+        Row(1067, "2017-07-01 00:16:31", "2017-07-01 00:34:19", 448, "W 37 St & 10 Ave", 40.75660359, -73.9979009, 487, "E 20 St & FDR Drive", 40.73314259, -73.97573881, 27084, "Subscriber", 1990, 2.0, 1.99)
       )
 
       transformedDF.schema("distance") should be(StructField("distance", DoubleType, nullable = true))
