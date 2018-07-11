cedec10825f53255bde4ee026b0185c98a169032
Chandni Patel <chandnir.patel@gmail.com>
2018-07-11 15:47:58 +0100

    [Chandni] Adding config to test spark session so that it works on people's computers who don't have hostnames that end in something besides local don't run into issues.

--- tw-pipeline/src/test/scala/com/thoughtworks/ca/de/DefaultFeatureSpecWithSpark.scala
+++ tw-pipeline/src/test/scala/com/thoughtworks/ca/de/DefaultFeatureSpecWithSpark.scala
@@ -4,5 +4,9 @@ import org.apache.spark.sql.SparkSession
 import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
 
 class DefaultFeatureSpecWithSpark extends FeatureSpec with GivenWhenThen with Matchers {
-  val spark: SparkSession = SparkSession.builder.appName("Spark Test App").master("local").getOrCreate()
+  val spark: SparkSession = SparkSession.builder
+    .appName("Spark Test App")
+    .config("spark.driver.host","127.0.0.1")
+    .master("local")
+    .getOrCreate()
 }
