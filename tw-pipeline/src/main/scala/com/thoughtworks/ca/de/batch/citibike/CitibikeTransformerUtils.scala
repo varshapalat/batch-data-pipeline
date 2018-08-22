01a6ecd4f076c5202e0026808414a852542651b0
Yuan <zhangyuan@users.noreply.github.com>
2018-08-22 15:51:41 +0800

    [zhangyuan] fix the serialization issue in citibike

--- tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/citibike/CitibikeTransformerUtils.scala
+++ tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/citibike/CitibikeTransformerUtils.scala
@@ -3,12 +3,13 @@ package com.thoughtworks.ca.de.batch.citibike
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 object CitibikeTransformerUtils {
-  implicit class StringDataset(val dataSet: Dataset[Row]) {
-    private final val MetersPerFoot = 0.3048
-    private final val FeetPerMile = 5280
+  private final val MetersPerFoot = 0.3048
+  private final val FeetPerMile = 5280
 
-    final val EarthRadiusInM: Double = 6371e3
-    final val MetersPerMile: Double = MetersPerFoot * FeetPerMile
+  final val EarthRadiusInM: Double = 6371e3
+  final val MetersPerMile: Double = MetersPerFoot * FeetPerMile
+
+  implicit class StringDataset(val dataSet: Dataset[Row]) {
 
     def computeDistances(spark: SparkSession) = {
       dataSet
