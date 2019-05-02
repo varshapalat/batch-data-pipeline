faa904c3eea6bc25912679f8409ceec956b7b9d7
Varsha Palat <varsha.p.aux@go-jek.com>
2019-05-02 18:27:26 +0530

    Writing unit test for suicide data

--- tw-pipeline/src/test/scala/com/thoughtworks/ca/de/batch/suicides/SuicideData2Test.scala
+++ tw-pipeline/src/test/scala/com/thoughtworks/ca/de/batch/suicides/SuicideData2Test.scala
@@ -1,14 +1,31 @@
 package com.thoughtworks.ca.de.batch.suicides
 
-import org.apache.spark.sql.QueryTest
 import org.apache.spark.sql.test.SharedSQLContext
+import org.apache.spark.sql.{QueryTest, SparkSession}
 
 class SuicideData2Test extends QueryTest with SharedSQLContext {
 
-  import testImplicits._
 
   test("calculate total suicides in year by gender") {
     //Write Test
     //Hint: try using checkAnswer()
+    val spark: SparkSession = SparkSession.builder
+      .appName("Spark Test App")
+      .config("spark.driver.host","127.0.0.1")
+      .master("local")
+      .getOrCreate()
+    import spark.implicits._
+    val suicideDF = Seq((1990, "M", 20), (1990, "F", 20), (1991, "F", 20), (1989, "F", 20)).toDF("year", "sex", "suicides_no")
+
+    val actualOutput = SuicideData(suicideDF).totalSuicidesInYearByGender(1990)
+    val expectedOutput = Seq(
+      ("F", 20),
+      ("M", 20)
+    ).toDF( "sex", "sum(suicides_no)")
+
+    actualOutput.show()
+    expectedOutput.show()
+
+    checkAnswer(actualOutput, expectedOutput)
   }
 }
--- tw-pipeline/src/test/scala/com/thoughtworks/ca/de/batch/suicides/SuicideDataTest.scala
+++ tw-pipeline/src/test/scala/com/thoughtworks/ca/de/batch/suicides/SuicideDataTest.scala
@@ -9,6 +9,24 @@ class SuicideDataTest extends DefaultFeatureSpecWithSpark with Matchers {
     scenario("calculate total suicides in year by gender") {
       //Write Test
       //Hint: try using except()
+
+      import spark.implicits._
+
+      val suicideDF = Seq((1990, "M", 20), (1990, "F", 20), (1991, "F", 20), (1989, "F", 20)).toDF("year", "sex", "suicides_no")
+
+
+      val actualOutput = SuicideData(suicideDF).totalSuicidesInYearByGender(1990)
+      val expectedOutput = Seq(
+        ("F", 20),
+        ("M", 20)
+      ).toDF( "sex", "sum(suicides_no)")
+
+      actualOutput.show()
+      expectedOutput.show()
+      val extraElements1 = actualOutput.except(expectedOutput)
+      val extraElements2 = expectedOutput.except(actualOutput)
+      assert(extraElements1.count()==0)
+      assert(extraElements2.count()==0)
     }
   }
 }
