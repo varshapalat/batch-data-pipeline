a670158b3097e3be684b9f161c526ccefe8ccd5e
Varsha Palat <varsha.p.aux@go-jek.com>
2019-05-03 14:38:38 +0530

    word count

--- tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/wordcount/WordCountUtils.scala
+++ tw-pipeline/src/main/scala/com/thoughtworks/ca/de/batch/wordcount/WordCountUtils.scala
@@ -5,18 +5,25 @@ import org.apache.spark.sql.{Dataset, SparkSession}
 object WordCountUtils {
   implicit class StringDataset(val dataSet: Dataset[String]) {
     def splitWords(spark: SparkSession) = {
-      dataSet
+      import spark.implicits._
+      dataSet.flatMap(line => line.split(" "))
     }
 
     def countByWord(spark: SparkSession) = {
       import spark.implicits._
-      spark.emptyDataset[(String, Long)]
+      val df = dataSet.map(word => word.toLowerCase)
+        .map(word => word.replaceAll(",", ""))
+        .map(word => (word, 1))
+        .groupBy("_1")
+        .count()
+      df.as[(String, Long)]
+//      spark.emptyDataset[(String, Long)]
     }
   }
 
   implicit class StringLongDataset(val dataSet: Dataset[(String, Long)]) {
     def sortByWord(spark: SparkSession) = {
-      dataSet
+      dataSet.sort("_1")
     }
   }
 }
